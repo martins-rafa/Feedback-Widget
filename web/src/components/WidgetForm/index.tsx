7d41419e558c7e0214a78cb6a352288598011d72
Rafa Martins <rafamartins.dev@gmail.com>
2022-05-03 12:17:59 -0300

    widget form

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problem",
+    image: {
+      source: bugImageUrl,
+      alt: "Illustration of a bug",
+    },
+  },
+  IDEA: {
+    title: "Idea",
+    image: {
+      source: ideaImageUrl,
+      alt: "Illustration of a light bulb",
+    },
+  },
+  OTHER: {
+    title: "Other",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Illustration of a blue thought ballon",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        <span>
+          Made with ♥ by
+          <a
+            className="underline underline-offset-2"
+            href="https://twitter.com/devrafamartins"
+            target="_blank"
+          >
+            {" "}
+            Rafa Martins
+          </a>
+        </span>
+      </footer>
+    </div>
+  );
+}
