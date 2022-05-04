31672bdb56232c0523d9990bd54a691ff067ed98
Rafa Martins <rafamartins.dev@gmail.com>
2022-05-04 11:55:42 -0300

    send email services

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "2f3ef01ca09a6e",
+    pass: "653129a8da45ae",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Rafa Martins <rafamartins.dev@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
